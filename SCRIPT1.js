fc1e2c8600f8e23d8b88b5441a1a697fde7f2e55
PMustiga <pedromustiga20@gmail.com>
2025-04-29 22:05:26 -0500

    Edit 29-04

--- SCRIPT1.js
+++ SCRIPT1.js
@@ -1,8 +1,32 @@
+let VarGlobal1="texto de variable global";
 function FCambioColor(){
     let vTexto1 = document.getElementById("ejmDOM1");
     vTexto1.textContent= "Texto cambiado por Función FCambioColor";
     vTexto1.style.color="blue";
     vTexto1.insertAdjacentHTML("beforeEnd",
-        "<br>Este texto es adicional </br>");
+                             "<br>Este texto es adicional </br>");
     vTexto1.style.color="green";
+    vTexto1.style.backgroundColor="red";
+
+    vTexto1.insertAdjacentHTML("beforeEnd", VarGlobal1);
+    //la variable global se puede utilizar dentro de cualquier función
+
+}
+vTexto1= "Texto fuera de función"; //vTexto1 no es la variable de la función
+console.log(vTexto1);
+console.log(varTemporal); // varTemporal no está definido afuera de función
+
+function FCambioClase1(){
+    let vTexto2=document.getElementsByClassName("classDOM1");
+    vTexto2[0].innerText="Elemento [0] del arreglo \n de clase classDOM1";
+    vTexto2[0].style.color="white";
+    console.log("tamaño del array=",vTexto2.length);
+    console.log("Tipo de variabe vTexto2", typeof vTexto2[1]);
+    //se coloca el índice 0 por ser el primer elemento de la clase classDOM1 de la página web
+}
+
+function FCambioTag1(){
+    let vTexto3=document.getElementsByTagName("h2");
+    vTexto3[1].innerHTML="Elemento h2 cambiado por TagDOM1";
+        //se coloca el índice 1 por ser el segundo elemento h2  de la web index.html
 }
